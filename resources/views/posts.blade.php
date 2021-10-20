0c58e777d2e374b845a40e7ef114257ec8208f95
kylym1631 <67018582+kylym1631@users.noreply.github.com>
2021-10-20 20:59:55 +0200

    basic route, constraints,slug

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"></script>
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My first post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Alias architecto cupiditate doloribus eos et fuga fugiat magni
+            maxime minima modi molestias, non officia pariatur quae saepe suscipit,
+            veritatis voluptatibus. Voluptas?
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">My second post </a></h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Alias architecto cupiditate doloribus eos et fuga fugiat magni
+            maxime minima modi molestias, non officia pariatur quae saepe suscipit,
+            veritatis voluptatibus. Voluptas?
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">My third post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Alias architecto cupiditate doloribus eos et fuga fugiat magni
+            maxime minima modi molestias, non officia pariatur quae saepe suscipit,
+            veritatis voluptatibus. Voluptas?
+        </p>
+    </article>
+</body>
+</html>
