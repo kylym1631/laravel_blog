e9ea62b11e681668cc54f3a949295d64a3c741a5
kylym1631 <67018582+kylym1631@users.noreply.github.com>
2021-10-30 20:42:55 +0200

    design

---
+++ resources/views/_posts-header.blade.php
@@ -0,0 +1,55 @@
+<header class="max-w-xl mx-auto mt-20 text-center">
+    <h1 class="text-4xl">
+        Latest <span class="text-blue-500">Laravel From Scratch</span> News
+    </h1>
+
+    <h2 class="inline-flex mt-2">By Lary Laracore <img src="/images/lary-head.svg"
+                                                       alt="Head of Lary the mascot"></h2>
+
+    <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-4">
+        <!--  Category -->
+        <div class="relative lg:inline-flex items-center bg-gray-100 rounded-xl">
+
+            <x-dropdown>
+                <x-slot name="trigger">
+                    <button
+                        class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
+                            {{isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories'}}
+                        <x-icon name="down-arrow" class=" absolute pointer-events-none" style="right: 12px;"/>
+                    </button>
+                </x-slot>
+                <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
+                @foreach($categories as $category)
+                    <x-dropdown-item
+                        href="/categories/{{$category->slug}}"
+                        :active="isset($currentCategory) && $currentCategory->id == $category->id"
+                    >{{ucwords($category->name)}}
+                    </x-dropdown-item>
+{{--                    {{isset($currentCategory) && $currentCategory->id == $category->id ? 'bg-blue-500 text-white':''}}--}}
+                @endforeach
+            </x-dropdown>
+
+        </div>
+
+        <!-- Other Filters -->
+{{--        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">--}}
+{{--            <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">--}}
+{{--                <option value="category" disabled selected>Other Filters--}}
+{{--                </option>--}}
+{{--                <option value="foo">Foo--}}
+{{--                </option>--}}
+{{--                <option value="bar">Bar--}}
+{{--                </option>--}}
+{{--            </select>--}}
+
+{{--        </div>--}}
+
+        <!-- Search -->
+        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
+            <form method="GET" action="#">
+                <input type="text" name="search" placeholder="Find something"
+                       class="bg-transparent placeholder-black font-semibold text-sm">
+            </form>
+        </div>
+    </div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,10 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
     return view('posts',[
-        'posts'=>Post::latest('published_at')->get()
+        'posts'=>Post::latest('published_at')->get(),
+        'categories'=>Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}',function (Post $post){
 
@@ -35,13 +36,16 @@ Route::get('posts/{post:slug}',function (Post $post){
 Route::get('categories/{category:slug}',function (Category $category)
 {
     return view('posts',[
-        'posts'=>$category->posts
+        'posts'=>$category->posts,
+        'currentCategory'=>$category,
+        'categories'=>Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}',function (User $author)
 {
     return view('posts',[
-        'posts'=>$author->posts
+        'posts'=>$author->posts,
+        'categories'=>Category::all()
     ]);
 });
