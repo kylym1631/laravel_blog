824300528607a82547841b5bafdf67c550b08fe4
kylym1631 <67018582+kylym1631@users.noreply.github.com>
2021-11-01 00:19:24 +0100

    filtering

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,10 +16,28 @@ class Post extends Model
     public function scopeFilter($query, array $filters)
     {
         $query->when($filters['search'] ?? false,function ($query, $search){
-            $query
+            $query->where(function ($query) use ($search) {
+                $query
                 ->where('title','like','%'.$search . '%')
-                ->orWhere('body','like','%'.$search . '%');
+                    ->orWhere('body','like','%'.$search . '%');
+            });
+
         });
+
+        $query->when($filters['category'] ?? false, function ($query, $category)
+        {
+            $query->whereHas('category',function ($query) use ($category) {
+                $query->where('slug',$category);
+            });
+        });
+
+        $query->when($filters['author'] ?? false, function ($query, $author)
+        {
+            $query->whereHas('author',function ($query) use ($author) {
+                $query->where('username',$author);
+            });
+        });
+
     }
 
     public function category()
